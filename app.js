686d3bf558f8c869ab7d411ce8abf0c3af54ba21
Taisas <tisashi15@ad.aol.aoltw.net>
2016-12-06 18:08:02 +0900

    'APad追加'

--- app.js
+++ app.js
@@ -12,14 +12,24 @@ game.onload = function(){
     ground.x = 0;
     ground.y = 562 - 85;
     game.rootScene.addChild(ground);
-    game.keybind(90, 'a');
+    game.keybind(80, 'a');
     game.rootScene.backgroundColor = 'black';
 
     var pad = new APad();
     pad.y = 454;
     game.rootScene.addChild(pad);
 
-    var player2 = new Player(500, 200, pad);
+    var player2 = new Player(200, 350, pad);
+
+    document.addEventListener('keydown',function (e) {
+        player2.getAction(e.key);
+    });
+
+    var bar = new Bar(10, 10);
+    // bar.image = game.assets['bar.png'];
+    game.rootScene.addChild(bar);
+    bar.maxvalue = 300;
+    bar.value = bar.maxvalue;
 
 };
 
@@ -30,34 +40,37 @@ var Player = Class.create(Sprite, {
         this.x = x;
         this.y = y;
         this.pad = pad;
+        this.flight = false;
+        this.speed = 5;
         game.rootScene.addChild(this);
     },
     onenterframe: function(){
-        console.log(this.pad.vx);
-        if(game.input.a){
+        // console.log(this.pad.vx);
+        if(this.flight){
             this.frame = [2];
-            if(game.input.right){
-                this.scaleX = 1;
-                this.x += 5;
-            }else if(game.input.left){
-                this.x -= 5;
-            }else if(game.input.up){
-                this.y -= 5;
-            }else if(game.input.down){
-                if(this.y < 562 - GROUND_HEIGHT - 64){
-                    this.y += 5;
-                }else{
-                    this.y = 562 - GROUND_HEIGHT - 64;
-                }
+            this.scaleX = 1;
+            if(this.y <= 562 - GROUND_HEIGHT - 64){
+                this.x += this.speed * this.pad.vx * 2;
+                this.y += this.speed * this.pad.vy * 2;
+            }else{
+                this.y = 562 - GROUND_HEIGHT - 64;
             }
-        }else if(game.input.right){
+        }else if(this.pad.vx > 0){
             this.frame = [0, 0, 0, 1, 1, 1];
             this.scaleX = 1;
-            this.x += 5;
-        }else if(game.input.left){
+            if(this.x <= 1000 - 64 - this.speed){
+                this.x += this.speed;
+            }else{
+                this.x = 1000 - 64;
+            }
+        }else if(this.pad.vx < 0){
             this.frame = [0, 0, 0, 1, 1, 1];
             this.scaleX = - 1;
-            this.x -= 5;
+            if(this.x >= this.speed){
+                this.x -= this.speed;
+            }else{
+                this.x = 0;
+            }
         }else{
             this.frame = [0];
             if(this.y < 562 - GROUND_HEIGHT - 64){
@@ -66,6 +79,9 @@ var Player = Class.create(Sprite, {
                 this.y = 562 - GROUND_HEIGHT - 64;
             }
         }
+    },
+    getAction : function (a) {
+        if(a == 'm') this.flight = !this.flight;
     }
 });
 
